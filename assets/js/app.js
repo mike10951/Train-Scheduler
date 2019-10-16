b357ad259d7b1b522fc7e5018e0d05932cfe75f0
Miguel Cabrera <miguel.cabrera.1095@gmail.com>
2019-10-16 12:40:32 -0500

    Added basic file structure and website layout

---
+++ assets/js/app.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+apiKey: "AIzaSyAUy_Dv54AKwN8hSIzuY8rzgrexiKAc1G8",
+authDomain: "trainscheduler-ecdb5.firebaseapp.com",
+databaseURL: "https://trainscheduler-ecdb5.firebaseio.com",
+projectId: "trainscheduler-ecdb5",
+storageBucket: "trainscheduler-ecdb5.appspot.com",
+messagingSenderId: "992683033815",
+appId: "1:992683033815:web:e0bd2e39f7f4a988971c92"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
